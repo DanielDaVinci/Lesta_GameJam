e74f014aa3f65f35e88ee08d2f8b49ac04f03c7f
DESKTOP-DB9OINO\Alex <andrey.abramov2001@gmail.com>
2023-11-04 17:37:06 +0300

    Pickup interact visibility trigger

--- Source/GameJam/Private/Pickable/PowerPickup.cpp
+++ Source/GameJam/Private/Pickable/PowerPickup.cpp
@@ -6,7 +6,7 @@
 // Sets default values
 APowerPickup::APowerPickup()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	
 	PrimaryActorTick.bCanEverTick = true;
 
 }
